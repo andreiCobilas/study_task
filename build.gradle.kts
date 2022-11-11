6c20ffe9a74a9cf75b3daf666fcde36c42429f0f
andreicobilas <andrei7xd@yahoo.com>
2022-11-11 09:54:35 +0200

    Fixed Dockerfile settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.data:spring-data-relational:2.4.5")
 	implementation("io.springfox:springfox-boot-starter:3.0.0")
-//	implementation("io.springfox:springfox-swagger-ui:3.0.0")
-//	implementation("io.springfox:springfox-swagger2:3.0.0")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
--- dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:17-alpine3.13
-WORKDIR /app
-EXPOSE 8080
-ADD ./build/libs/products-0.0.1-SNAPSHOT.jar .
-CMD ["java", "-jar", "products-0.0.1-SNAPSHOT.jar"]
